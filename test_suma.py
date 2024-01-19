9865938413381ee3fced9a5fc44f723b02116a41
Manuel <manuelsrey@gmail.com>
2024-01-19 17:10:22 +0100

    Error corregido

--- test_suma.py
+++ test_suma.py
@@ -5,7 +5,7 @@ class TestSumar(unittest.TestCase):
 
     def test_sumar(self):
         self.assertEqual(sumar(3, 2), 5)
-        self.assertEqual(sumar(-1, 2), 0)
+        self.assertEqual(sumar(-1, 1), 0)
         self.assertEqual(sumar(-1,-1), -2)
 
 if __name__ == '__main__':
